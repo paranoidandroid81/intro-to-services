382150ab724626fc9b0318870a8f78939ddecc47
paranoidandroid81 <mpk44@pitt.edu>
2022-05-23 15:42:50 -0400

    demo2 + 3 + 4 naked

---
+++ dockerstuff/demo3/src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from a C app running in Docker on Linux again, this time with Alpine!\n");
+    return 0;
+}
---
+++ dockerstuff/demo4 - naked/src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from a C app running in Docker on Linux again, this time with Scratch!\n");
+    return 0;
+}
