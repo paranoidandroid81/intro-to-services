6df3804655dc13765a660448ce7f167dac4a479d
paranoidandroid81 <mpk44@pitt.edu>
2022-05-24 13:38:29 -0400

    Lunch 2nd day

---
+++ backend/ScheduleSolution/ScheduleAPI/Program.cs
@@ -0,0 +1,27 @@
+// WebApplication is Kestrel
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ConfigureServices in Startup -- setting up the stuff that happens behinds the scenes before we start
+var app = builder.Build();
+// all "middleware" - the stuff here sees the incoming requests and outgoing responses
+
+// Configure Method Startup
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run(); // blocking forever while loop
